7daf2536afac1702a629c584b6d24cb817bb1613
FrankSauve <franksauve@outlook.com>
2020-08-26 20:39:34 -0400

    Initial commit with movement and player attacking

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Animator animator;
+    public GameObject enemyObject;
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        // Play hurt animation
+        animator.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Die animation
+        animator.SetBool("IsDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        Destroy(enemyObject, 3.0f);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject heavyBandit;
+
+    float floorLevel = -4.06f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(heavyBandit, new Vector3(4, floorLevel, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
